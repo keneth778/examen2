4343f5b0e3c2fce873e188f5c6054ee58b248d7a
keneth <keneth.javier90@gmail.com>
2024-07-11 11:37:43 -0600

    section

--- section/section.js
+++ section/section.js
@@ -1,8 +1,8 @@
 let DOM_section = document.querySelector(".section");
 DOM_section.innerHTML = `
 <div class="grid-principal">
-<div class="grid"></div>
-<div class="grid"></div>
-<div class="grid"></div>
+<div class="grid1"></div>
+<div class="grid2"></div>
+<div class="grid3"></div>
 </div>
 `
