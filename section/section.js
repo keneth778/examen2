80a2a02cc0daf4ec31a500e0434d3e2541d731c9
keneth <keneth.javier90@gmail.com>
2024-07-11 11:00:00 -0600

    section

---
+++ section/section.js
@@ -0,0 +1,8 @@
+let DOM_section = document.querySelector(".section");
+DOM_section.innerHTML = `
+<div class="grid-principal">
+<div class="grid"></div>
+<div class="grid"></div>
+<div class="grid"></div>
+</div>
+`
