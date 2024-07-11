f682d3a7d216f74be9728d6b5fe863de453b3705
keneth <keneth.javier90@gmail.com>
2024-07-11 10:48:46 -0600

    header

---
+++ header/header.js
@@ -0,0 +1,7 @@
+let DOM_header = document.querySelector(".header");
+DOM_header.innerHTML = `
+<div class="flexbox">
+    <div class="usuario">usuario@</div>
+    <div class="git">git hub</div>
+</div>
+`
